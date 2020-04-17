96515c81c424d4d19c1dfcdbf37ec183e421af3a
Gareth <gtrufitt@gmail.com>
2020-04-17 13:01:39 +0100

    Everything

---
+++ cron.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node 
+const run = require('./index');
+
+const cron = require('node-cron');
+const repoNames = ['dotcom-rendering', 'discussion-rendering', 'frontend'];
+ 
+console.log('Scheduling');
+run(1, repoNames);
+
+cron.schedule('30 9-17/3 * * 1-5', () => { //“At minute 30 past every 3rd hour from 9 through 17 on every day-of-week from Monday through Friday.”
+    console.log('Running')
+    run(1, repoNames);
+});
+
+cron.schedule('0 9 * * *', () => { // Every Monday 9am
+    console.log('Running')
+    run(7, repoNames);
+});
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const showdown = require('showdown');
 const { Liquid } = require('liquidjs');
 const fs = require('fs');
+const open = require('open');
 
 
 const converter = new showdown.Converter();
@@ -13,31 +14,40 @@ var engine = new Liquid({
     extname: '.liquid'          // used for layouts/includes, defaults ""
 });
 
-const filterByDate = (json) => {
+const filterByDate = (json, duration) => {
+    console.log(json)
    return json.filter(pr => {
         const date = new Date(pr.created_at)
         const dateNow = new Date();
         const oneDay = 1000*60*60*24;
-        if ((dateNow - date) / oneDay < 7) {
+        if ((dateNow - date) / oneDay < duration) {
             return pr;
         }
     })
 }
 
 const getDescriptions = json => 
-    json.map(pr => ({ body: converter.makeHtml(pr.body), created: pr.created_at, title: pr.title, url: pr.html_url }))
+    json.map(pr => ({
+        body: converter.makeHtml(pr.body),
+        created: pr.created_at,
+        title: pr.title,
+        url: pr.html_url,
+        repoName: pr.head.repo.name
+    }))
 
 const generateHtml = json => 
 engine
     .renderFile("email", json)
 
-
-fetch('https://api.github.com/repos/guardian/dotcom-rendering/pulls?state=all')
-  .then(res => res.json())
-    .then(filterByDate)
+module.exports = (duration, repoNames) => {
+    Promise.all(repoNames.map(val => fetch(`https://api.github.com/repos/guardian/${val}/pulls?state=all`).then(res => res.json())))
+    .then(res => res.reduce((prev, current) => prev.concat(current))) // Take all (json converted) response and combine them into one array 
+    .then(json => filterByDate(json, duration))
     .then(getDescriptions)
-    .then(json => ({ prs: json }))
-    .then(json => { console.log(json); return json})
-    .then(generateHtml)
+    .then(json => ({ prs: json })) // convert to use in liquid
+    .then(json => { console.log(json); return json}) // Just log
+    .then(generateHtml) // pass to liquid template
     .then(html => fs.writeFile('email.html', html, e => console.log))
+    .then(() => open('email.html'))
   .catch(e => console.log(e))
+}
